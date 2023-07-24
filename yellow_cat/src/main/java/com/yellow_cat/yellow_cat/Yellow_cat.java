05ee982ea78f83fe8c75be6100c8feb969d6af5d
Anfe-ion <andres.moncada.c@gmail.com>
2023-07-23 23:01:37 -0500

    Se añade el proyecto Java.

---
+++ yellow_cat/src/main/java/com/yellow_cat/yellow_cat/Yellow_cat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.yellow_cat.yellow_cat;
+
+/**
+ *
+ * @author Andrés
+ */
+public class Yellow_cat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
